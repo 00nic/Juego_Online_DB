52a01269c08158cf06dce716907bf0bff81ff2f9
00nic <nicoenzogon@gmail.com>
2026-05-27 19:57:41 -0300

    respuestas

--- ejercicios/ejercicios sql avanzados COMISION 4.sql
+++ ejercicios/ejercicios sql avanzados COMISION 4.sql
@@ -18,43 +18,67 @@ MODELO DE DATOS:
 -- 1.1 Selecciona todos los jugadores con nivel superior a 10
 -- Tu respuesta:
 
+SELECT * FROM jugadores where nivel > 10
 
 -- 1.2 Muestra los nombres de los clanes creados después del 1 de enero de 2021
 -- Tu respuesta:
 
+SELECT nombre_clan FROM clanes where fecha_creacion > '2021-01-01' |
 
 -- 1.3 Lista todos los equipamientos de tipo "arma" con nivel requerido menor a 10
 -- Tu respuesta:
 
+SELECT * FROM equipamientos where tipo = 'arma' and nivel_requerido < 10
 
 -- 1.4 Encuentra todas las misiones de dificultad "Muy Difícil"
 -- Tu respuesta:
 
+SELECT * FROM misiones where dificultad = 'Muy Difícil'
 
 -- 1.5 Selecciona los monstruos de tipo "dragón" con más de 500 de vida
 -- Tu respuesta:
 
+USE juego_online;
+SELECT *
+FROM monstruos
+WHERE tipo_monstruo = 'Dragón' and vida > 5000
 
 -- 1.6 Actualiza la experiencia del jugador "Arthorius" sumándole 500 puntos
 -- Tu respuesta:
+
+UPDATE jugadores
+SET experiencia = experiencia + 500
+WHERE nombre_jugador = 'Arthorius';
 
 
 -- 1.7 Elimina todas las misiones completadas antes del 1 de enero de 2024
 -- (de la tabla jugadores_misiones)
 -- Tu respuesta:
 
+DELETE FROM jugadores_misiones
+WHERE fecha_completada < '2024-01-01' and completada = true;
 
 -- 1.8 Muestra los 5 jugadores con mayor nivel, ordenados descendentemente
 -- Tu respuesta:
 
+SELECT nombre_jugador, nivel
+FROM jugadores
+ORDER BY nivel DESC
+LIMIT 5;
 
 -- 1.9 Cuenta cuántos jugadores hay en total
 -- Tu respuesta:
+
+SELECT COUNT(*) AS total_jugadores
+FROM jugadores; 
 
 
 -- 1.10 Lista los nombres de jugadores que contengan la letra "a" (mayúscula o minúscula)
 -- Tu respuesta:
 
+SELECT nombre_jugador
+FROM jugadores
+WHERE nombre_jugador LIKE '%a%';
 
 -- =====================================================
 -- SECCIÓN 2: OPERACIONES INTERMEDIAS 
@@ -66,45 +90,91 @@ MODELO DE DATOS:
 --     - Nivel 11-15: "Avanzado"
 --     - Más de 15: "Maestro"
 -- Tu respuesta:
-
+SELECT nombre_jugador, nivel,
+CASE
+    WHEN nivel BETWEEN 1 AND 5 THEN 'Novato'
+    WHEN nivel BETWEEN 6 AND 10 THEN 'Intermedio'
+    WHEN nivel BETWEEN 11 AND 15 THEN 'Avanzado'
+    ELSE 'Maestro'
+END AS clasificacion
+FROM jugadores
+ORDER BY nivel ASC;
 
 -- 2.2 Cuenta cuántos jugadores hay en cada clan
 -- Tu respuesta:
 
+SELECT clan_id, c.nombre_clan, COUNT(clan_id) as cantidad_jugadores
+FROM jugadores j
+INNER JOIN clanes c ON c.id_clan = j.clan_id
+GROUP BY clan_id
+ORDER BY cantidad_jugadores 
 
 -- 2.3 Muestra los clanes que tienen más de 4 jugadores
--- Tu respuesta:
+-- Tu respuesta
 
+SELECT clan_id, c.nombre_clan, COUNT(clan_id) as cantidad_jugadores
+FROM jugadores j
+INNER JOIN clanes c ON c.id_clan = j.clan_id
+GROUP BY clan_id
+HAVING cantidad_jugadores > 4
+ORDER BY cantidad_jugadores 
 
 -- 2.4 Calcula el nivel promedio, máximo y mínimo de todos los jugadores
 -- Tu respuesta:
 
+SELECT round(AVG(nivel)) AS nivel_promedio, 
+MIN(nivel) AS nivel_mínimo, 
+MAX(nivel) AS nivel_máximo
+FROM jugadores
 
 -- 2.5 Encuentra el equipamiento con mayor bono de ataque
 -- Tu respuesta:
  
+SELECT *
+FROM equipamientos
+WHERE bono_ataque = (SELECT MAX(bono_ataque) FROM equipamientos)
 
 -- 2.6 Lista todos los tipos de monstruos únicos (sin duplicados)
 -- Tu respuesta:
+
+SELECT DISTINCT tipo_monstruo
+FROM monstruos
 
 
 -- 2.7 Muestra las misiones ordenadas por recompensa de oro (mayor a menor)
 -- y luego por recompensa de experiencia (mayor a menor)
 -- Tu respuesta:
 
-
+SELECT *
+FROM misiones
+ORDER BY recompensa_oro DESC, recompensa_xp DESC
 
 -- 2.8 Calcula la experiencia total y promedio por clan
 -- Tu respuesta:
 
+SELECT nombre_clan, SUM(experiencia) as Total_exp_clan, AVG(experiencia) as exp_prom_clan
+FROM clanes c 
+INNER JOIN jugadores j ON c.id_clan = j.clan_id
+group by nombre_clan
 
 -- 2.9 Encuentra los jugadores que NO tienen clan asignado (clan_id es NULL)
 -- y muestra "Sin Clan" en lugar de NULL
 -- Tu respuesta:
 
+SELECT id_jugador, nombre_jugador, nivel, experiencia, COALESCE(clan_id, 'Sin Clan') as clan_id
+FROM jugadores
+WHERE clan_id IS NULL 
+
+SELECT id_jugador, nombre_jugador, nivel, experiencia, IFNULL(clan_id, 'Sin Clan') as clan_id
+FROM jugadores
+WHERE clan_id IS NULL 
 
 -- 2.10 Cuenta cuántas misiones hay de cada nivel de dificultad
 -- Tu respuesta:
+
+SELECT dificultad, COUNT(id_mision) as cant_misiones
+FROM misiones
+GROUP BY dificultad
 
 
 -- =====================================================
@@ -114,20 +184,42 @@ MODELO DE DATOS:
 -- 4.1 INNER JOIN: Muestra todos los jugadores con el nombre de su clan
 -- Tu respuesta:
 
+SELECT * 
+FROM jugadores j
+INNER JOIN clanes c
+ON j.clan_id = c.id_clan
+
 
 -- 4.2 LEFT JOIN: Muestra todos los clanes y cuántos jugadores tienen
 -- (incluye clanes sin jugadores)
 -- Tu respuesta:
 
+SELECT nombre_clan, COUNT(id_jugador) as cantidad_jugadores
+FROM clanes c
+LEFT JOIN jugadores j
+ON j.clan_id = c.id_clan
+GROUP BY id_clan
 
 -- 4.3 Muestra todas las misiones con el nombre del monstruo que hay que derrotar
 -- Tu respuesta:
 
+SELECT mi.nombre_mision, mo.nombre_monstruo
+FROM misiones mi
+INNER JOIN monstruos mo
+ON mi.monstruo_id = mo.id_monstruo
 
 -- 4.4 Lista todos los jugadores con sus misiones completadas
 -- mostrando: nombre jugador, nombre misión, fecha completada
 -- Tu respuesta:
 
+SELECT nombre_jugador, nombre_mision, fecha_completada
+FROM jugadores j
+INNER JOIN jugadores_misiones ju
+ON j.id_jugador = ju.id_jugador
+INNER JOIN misiones m
+ON ju.id_mision = m.id_mision
+WHERE ju.completada = True
+ORDER BY fecha_completada
 
 -- 4.5 Muestra el equipamiento que tiene cada jugador
 -- incluyendo: nombre jugador, nombre equipamiento, cantidad
